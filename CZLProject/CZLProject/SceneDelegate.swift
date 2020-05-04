636c7e7fd5182f5e4c2e76d42e00d48c8c791d78
DerenChen <deren_ch@163.com>
2020-05-04 16:56:59 +0800

    提交

--- CZLProject/CZLProject/SceneDelegate.swift
+++ CZLProject/CZLProject/SceneDelegate.swift
@@ -18,6 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene as! UIWindowScene)
+        window?.backgroundColor = UIColor.orange
+        window?.rootViewController = CZLBaseViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
