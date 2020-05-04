4860d32428f6e973ade647cefb8b365e528df712
DerenChen <deren_ch@163.com>
2020-05-04 15:50:23 +0800

    提交： 项目创建

---
+++ CZLProject/CZLProject/Classes/Tools/Extensions/UIScreen+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIScreen+Extensions.swift
+//  CZLProject
+//
+//  Created by yuzeux on 2020/5/4.
+//  Copyright © 2020 strloong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIScreen {
+    static let czl_screenRect: CGRect = UIScreen.main.bounds
+    static let czl_screenWidth: CGFloat = UIScreen.main.bounds.width
+    static let czl_screenHeight: CGFloat = UIScreen.main.bounds.height
+}
---
+++ CZLProject/CZLProject/Classes/View/Main/CZLBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CZLBaseViewController.swift
+//  CZLProject
+//
+//  Created by yuzeux on 2020/5/4.
+//  Copyright © 2020 strloong. All rights reserved.
+//
+
+import UIKit
+
+class CZLBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
